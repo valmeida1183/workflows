a9958f140769d66078bbf86555e17e48daceec29
Thiago Lunardi <thl@templafy.com>
2024-08-02 16:54:06 +0200

    Update app version retrieval logic

--- src/api/TodoEndpointsExtensions.cs
+++ src/api/TodoEndpointsExtensions.cs
@@ -165,7 +165,7 @@ namespace SimpleTodo.Api
 
         public static IResult GetAppVersion(IConfiguration configuration)
         {
-            var appVersionRaw = configuration["FRAMEWORK_VERSION"] ?? "v0.0.1-rc";
+            var appVersionRaw = Environment.GetEnvironmentVariable("FRAMEWORK_VERSION") ?? "v0.0.1-rc";
             var appVersion = Regex.Match(appVersionRaw, @"v[^:]+$").Value;
 
             return TypedResults.Ok(new AppVersion(appVersion));
